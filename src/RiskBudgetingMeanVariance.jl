0b84717ca898f6519c8fd7e0f79d9f64e235b391
Bernardo Freitas Paulo da Costa <bernardofpc@im.ufrj.br>
2023-10-05 11:25:45 -0300

    Add Interpolating RB code

--- src/RiskBudgetingMeanVariance.jl
+++ src/RiskBudgetingMeanVariance.jl
@@ -1,10 +1,13 @@
 module RiskBudgetingMeanVariance
 
+using LinearAlgebra
+using Convex, ECOS
 using JuMP, Ipopt
 solver = JuMP.optimizer_with_attributes(Ipopt.Optimizer, "print_level" => 0)
 
 include("markowitz.jl")
+include("gauss_rb.jl")
 
-export mmv_lambda, mmv_return, mmv_vol
+export mmv_lambda, mmv_return, mmv_vol, rb_ws
 
 end # module RiskBudgetingMeanVariance
---
+++ src/gauss_rb.jl
@@ -0,0 +1,83 @@
+# TODO: check solution for feasibility
+"""
+    rb_ws_jump(means, covs, B; min_ret=nothing, max_vol=nothing)
+
+Weights (positive, summing 1) of the interpolating Risk-Budgeting
+portfolio corresponding to volatility contributions B_i.
+One can also set minimum return and maximum volatility of the resulting
+portfolio, in which case it will not be strictly Risk-Budgeting.
+
+means is the mean loss of each asset (so is typically negative),
+covs is the covariance matrix of losses.
+"""
+function rb_ws_jump(means, covs, B; min_ret=nothing, max_vol=nothing)
+  # Aux
+  dim = length(means)
+
+  # Base model
+  m = Model(solver)
+  @variable(m, w[1:dim] >= 0)
+  @NLconstraint(m, sum(B[i] * log(w[i]) for i=1:dim) >= 0)
+
+  @expression(m, mean_loss, means' * w)
+  register(m, :f_std, dim, (ws...) -> std_port(covs, ws...); autodiff = true)
+  @NLexpression(m, std_loss, f_std(w...))
+
+  @objective(m, Min, std_loss)
+
+
+  # Add return / variance constraint
+  if min_ret != nothing
+    @constraint(m, ret_bound, -mean_loss >= min_ret * sum(m[:w]))
+  end
+  if max_vol != nothing
+    @constraint(m, std_bound, std_loss <= max_vol * sum(m[:w]) )
+  end
+
+  optimize!(m)
+  w_rb = value.(w)
+  return w_rb ./ sum(w_rb)
+end
+
+"""
+    rb_ws_cvx(means, covs, B; min_ret=nothing, max_vol=nothing)
+
+Weights (positive, summing 1) of the interpolating Risk-Budgeting
+portfolio corresponding to volatility contributions B_i.
+One can also set minimum return and maximum volatility of the resulting
+portfolio, in which case it will not be strictly Risk-Budgeting.
+
+means is the mean loss of each asset (so is typically negative),
+covs is the covariance matrix of losses.
+"""
+function rb_ws_cvx(means, covs, B; min_ret=nothing, max_vol=nothing)
+  # Aux
+  dim = length(means)
+  chol_cov = cholesky(covs)
+
+  # Convex model
+  w = Variable(dim, Positive())
+  rb_constr = sum(B[i] * log(w[i]) for i=1:dim) >= 0
+
+  port_loss = means' * w
+  port_vol  = norm(chol_cov.U * w)
+
+  constr = Convex.Constraint[rb_constr]
+  # Add return / variance constraint
+  if min_ret != nothing
+    push!(constr, -port_loss >= min_ret * sum(w) )
+  end
+  if max_vol != nothing
+    push!(constr, port_vol <= max_vol * sum(w) )
+  end
+
+  pb = minimize(port_vol, constr)
+
+  solve!(pb, ECOS.Optimizer; silent_solver=true)
+  w_rb = w.value[:]
+  return w_rb ./ sum(w_rb)
+end
+
+rb_ws = rb_ws_cvx
+
+
