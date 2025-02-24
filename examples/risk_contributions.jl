a6cafd61db8407b6c14af6a2d5955c718704efcc
Bernardo Freitas Paulo da Costa <bernardo.paulo@fgv.br>
2025-02-24 11:51:56 -0300

    Function for interpolation curve of RB 2 MMV

--- examples/risk_contributions.jl
+++ examples/risk_contributions.jl
@@ -34,6 +34,30 @@ function entropy(rc)
   return - sum(log(p_i)*p_i for p_i in ps if p_i > 0)
 end
 
+
+# RiskBudgetingMeanVariance curves
+
+function rb2mmv(means, covs; B=ones(length(means)), target_vol=0.1)
+  w_mmv = mmv_vol(means, covs, target_vol; positive=true)
+  w_rb = rb_ws(-means, covs, B)
+
+  ret_mmv = means' * w_mmv
+  ret_rb  = means' * w_rb
+
+  interpolation_curve = []
+  ws = []
+  for j in 0:20
+    print("$j, ")
+    target_ret = ret_rb + j/20*(ret_mmv - ret_rb)
+    w_rb_i = rb_ws(-means, covs, B; min_ret=target_ret, max_vol=0.1)
+    ret_i = rets' * w_rb_i
+    vol_i = sqrt(w_rb_i' * Covs * w_rb_i)
+    push!(interpolation_curve, (vol_i,ret_i))
+    push!(ws, w_rb_i)
+  end
+  return interpolation_curve, ws
+end
+
 #
 # Script
 #
@@ -73,6 +97,7 @@ end
 plt.figure()
 for i in 1:n_reps_int
   print("Rep $i: ")
+  # Sample returns to estimate means and covariances
   returns = rand(rng, normdist, N)
 
   means = sum(returns, dims=2)/N
@@ -80,25 +105,10 @@ for i in 1:n_reps_int
   errs  = returns .- means
   covs  = (errs * errs') / N
 
-  w_mmv = mmv_vol(means, covs, 0.1; positive=true)
-  w_rb = rb_ws(-means, covs, B)
+  interpolation_curve, ws = rb2mmv(means, covs; B=B, target_vol=0.1)
 
-  ret_mmv = means' * w_mmv
-  ret_rb  = means' * w_rb
-
-  int_curve = []
-  ws = []
-  for j in 0:20
-    print("$j, ")
-    target_ret = ret_rb + j/20*(ret_mmv - ret_rb)
-    w_rb_i = rb_ws(-means, covs, B; min_ret=target_ret, max_vol=0.1)
-    ret_i = rets' * w_rb_i
-    vol_i = sqrt(w_rb_i' * Covs * w_rb_i)
-    push!(int_curve, (vol_i,ret_i))
-    push!(ws, w_rb_i)
-  end
   global handle_sample, = plt.plot(0:20, [measure(risk_contributions(Covs, wi)) for wi in ws], color="C0", alpha=0.1, label="Samples")
-  plot_contributions && axs[i].stackplot(0:20, 100*hcat([risk_contributions(Covs, ws_i) for ws_i in ws]...))
+  plot_contributions && axs[i].stackplot(0:20, 100*hcat([risk_contributions(Covs, wi) for wi in ws]...))
 
   println()
 end
@@ -106,30 +116,15 @@ end
 # (True) Markowitz and RB portfolios
 
 begin
-  w_mmv = mmv_vol(rets, Covs, 0.1; positive=true)
-  w_rb = rb_ws(-rets, Covs, B)
+  interpolation_curve, ws = rb2mmv(rets, Covs; B=B, target_vol=0.1)
 
-  ret_mmv = rets' * w_mmv
-  ret_rb  = rets' * w_rb
-
-  int_curve = []
-  ws = []
-  for j in 0:20
-    print("$j, ")
-    target_ret = ret_rb + j/20*(ret_mmv - ret_rb)
-    w_rb_i = rb_ws(-rets, Covs, B; min_ret=target_ret, max_vol=0.1)
-    ret_i = rets' * w_rb_i
-    vol_i = sqrt(w_rb_i' * Covs * w_rb_i)
-    push!(int_curve, (vol_i,ret_i))
-    push!(ws, w_rb_i)
-  end
   global handle_pop, = plt.plot(0:20, [measure(risk_contributions(Covs, wi)) for wi in ws], color="C1", label="Population", linewidth=2.5)
   plt.title(title * " of risk contributions")
   plt.legend(handles=[handle_sample, handle_pop])
 
   if plot_contributions
     plt.figure() # only activate for stackplot
-    plt.stackplot(0:20, 100*hcat([risk_contributions(Covs, ws_i) for ws_i in ws]...))
+    plt.stackplot(0:20, 100*hcat([risk_contributions(Covs, wi) for wi in ws]...))
     plt.title("Evolution of risk contributions for population parameters")
   end
 
